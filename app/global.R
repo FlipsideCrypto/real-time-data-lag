de40486e24e6e6e7bda36ef48b2cf343e9f4dc8f
Carlos R. Mercado <107061601+charlieflipside@users.noreply.github.com>
2022-08-15 11:36:55 -0400

    v1 complete

--- app/global.R
+++ app/global.R
@@ -5,9 +5,22 @@ library(plotly)
 library(dplyr)
 library(reactable)
 
-example_data <- data.frame(
-  x = 1:100, 
-  y = abs(rnorm(100)),
-  z = abs(ceiling(rnorm(100))),
-  a = as.factor(sample(LETTERS[1:5], 100, replace = TRUE))
+query <- 
+  "
+with lag_info AS (
+SELECT 'ETHEREUM' as chain, MAX(BLOCK_TIMESTAMP) as latest_data, TIMESTAMPDIFF(minute, CURRENT_TIMESTAMP, MAX(BLOCK_TIMESTAMP)) as lag_minutes FROM ethereum.core.fact_transactions UNION 
+SELECT 'SOLANA' as chain, MAX(BLOCK_TIMESTAMP) as latest_data, TIMESTAMPDIFF(minute, CURRENT_TIMESTAMP, MAX(BLOCK_TIMESTAMP)) as lag_minutes FROM solana.core.fact_transactions UNION
+SELECT 'AVALANCHE' as chain, MAX(BLOCK_TIMESTAMP) as latest_data, TIMESTAMPDIFF(minute, CURRENT_TIMESTAMP, MAX(BLOCK_TIMESTAMP)) as lag_minutes FROM avalanche.core.fact_transactions UNION
+SELECT 'POLYGON' as chain, MAX(BLOCK_TIMESTAMP) as latest_data, TIMESTAMPDIFF(minute, CURRENT_TIMESTAMP, MAX(BLOCK_TIMESTAMP)) as lag_minutes FROM polygon.core.fact_transactions UNION
+SELECT 'FLOW' as chain, MAX(BLOCK_TIMESTAMP) as latest_data, TIMESTAMPDIFF(minute, CURRENT_TIMESTAMP, MAX(BLOCK_TIMESTAMP)) as lag_minutes FROM flow.core.fact_transactions UNION
+SELECT 'BSC' as chain, MAX(BLOCK_TIMESTAMP) as latest_data, TIMESTAMPDIFF(minute, CURRENT_TIMESTAMP, MAX(BLOCK_TIMESTAMP)) as lag_minutes FROM bsc.core.fact_transactions UNION
+SELECT 'NEAR' as chain, MAX(BLOCK_TIMESTAMP) as latest_data, TIMESTAMPDIFF(minute, CURRENT_TIMESTAMP, MAX(BLOCK_TIMESTAMP)) as lag_minutes FROM near.core.fact_transactions UNION
+SELECT 'GNOSIS' as chain, MAX(BLOCK_TIMESTAMP) as latest_data, TIMESTAMPDIFF(minute, CURRENT_TIMESTAMP, MAX(BLOCK_TIMESTAMP)) as lag_minutes FROM gnosis.core.fact_transactions UNION
+SELECT 'OPTIMISM' as chain, MAX(BLOCK_TIMESTAMP) as latest_data, TIMESTAMPDIFF(minute, CURRENT_TIMESTAMP, MAX(BLOCK_TIMESTAMP)) as lag_minutes FROM optimism.core.fact_transactions UNION
+SELECT 'ARBITRUM' as chain, MAX(BLOCK_TIMESTAMP) as latest_data, TIMESTAMPDIFF(minute, CURRENT_TIMESTAMP, MAX(BLOCK_TIMESTAMP)) as lag_minutes FROM arbitrum.core.fact_transactions
 )
+
+SELECT * FROM lag_info ORDER BY LAG_MINUTES ASC;
+"
+
+latest_data <- auto_paginate_query(query, readLines('api_key.txt'))
--- app/server.R
+++ app/server.R
@@ -1,94 +1,29 @@
 library(shiny)
 source("global.R")
 server <- function(input, output, session) {
+
+latest_run <- eventReactive(input$rerun, {
   
-  # example_data in global 
+  q <- shroomDK::create_query_token(query = query, 
+                               api_key = readLines('api_key.txt'),
+                               ttl = 1, cache = FALSE)
   
-  # Example SCATTERPLOT
-  output$exscatter <- renderPlotly({
-    plot_ly(data = example_data,
-            x = ~x,
-            y = ~y,
-            color = ~a,
-            size = ~z,
-            text = ~paste("Y: ", y,
-                          '<br>Size:', z,
-                          '<br>Color:', a)
-            ) %>% 
-            layout(title = "",
-                   yaxis = list(title = "Y Label", 
-                                showgrid = FALSE,
-                                color = "#FFF"), 
-                   xaxis = list(title = "X Label",
-                                showgrid = TRUE,
-                                color = "#FFF",
-                                gridcolor = "#202933"),
-                   plot_bgcolor = "transparent", 
-                   paper_bgcolor = "transparent",
-                   legend = list(font = list(color = '#FFFFFF'))) %>%
-      # variety of useful config options to be aware of
-      # https://plotly.com/r/configuration-options/
-              config(scrollZoom = FALSE,
-                     # displayModeBar = TRUE, # default is hover
-                     toImageButtonOptions = list(format= 'svg', # one of png, svg, jpeg, webp
-                                                 filename= 'template_image',
-                                                 height= 500,
-                                                 width= 700,
-                                                 scale= 1),
-                     displaylogo = FALSE
-              )
-                     
-    
-  })
+  q <- shroomDK::get_query_from_token(q$token, api_key = readLines('api_key.txt'))
   
-  # Example BARPLOT 
-  # more details - https://plotly.com/r/bar-charts/
-  output$exbar <- renderPlotly({
-    
-    ed <- example_data %>% 
-      group_by(a, z, .add = TRUE) %>% 
-      summarize(n = n())
-    
-    plot_ly(data = ed,
-            x = ~as.character(a),
-            y = ~n,
-            color = ~as.factor(z),
-            text = ~paste("Letter: ", a,
-                          '<br>Category:', z,
-                          '<br>Number:', n)
-    ) %>% 
-      layout(title = "",
-             yaxis = list(title = "Y Label", 
-                          showgrid = FALSE,
-                          color = "#FFF"), 
-             xaxis = list(title = "X Label",
-                          showticklabels = TRUE,
-                          color = "#FFF",
-                          gridcolor = "#202933"),
-             plot_bgcolor = "transparent", 
-             paper_bgcolor = "transparent",
-             legend = list(font = list(color = '#FFFFFF'))) %>%
-      # variety of useful config options to be aware of
-      # https://plotly.com/r/configuration-options/
-      config(scrollZoom = FALSE,
-             # displayModeBar = TRUE, # default is hover
-             toImageButtonOptions = list(format= 'svg', # one of png, svg, jpeg, webp
-                                         filename= 'template_image',
-                                         height= 500,
-                                         width= 700,
-                                         scale= 1),
-             displaylogo = FALSE
-      )
-    
-    
+  clean_query(q)
   })
   
   # Simple reactable - see: styles.css for formatting
   output$myreactable <- renderReactable({
-    reactable(example_data,
+    if(input$rerun == 0){
+    reactable(latest_data,
               borderless = TRUE,
-              outlined = FALSE
-                )
+              outlined = FALSE)
+      } else { 
+        reactable(latest_run(),
+                  borderless = TRUE,
+                  outlined = FALSE)        
+      }
   })
   
 
